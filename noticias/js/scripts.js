48ebf8e3127603a348e92b8a10a30451cbef313b
Amado <amadoabaca99@gmail.com>
2024-05-03 00:25:14 -0300

    Agregada API de noticias 'News API'

--- noticias/js/scripts.js
+++ noticias/js/scripts.js
@@ -1,57 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-    //REQUEST A LA API DE jSON PLACE HOLDER
-    //ENDPOINT A LOS POSTS DE EJEMPLO
-    const url= 'https://jsonplaceholder.typicode.com/posts'
-    //creamos un objeto del tipo request
-    const req = new Request(url);
-    //creamos encabezados para la peticion
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');  //AGREGAMOS AL HEADER QUE EL CONTENIDO SEA DEVUELTO EN FORMATO JSON
-    //generamos la peticion pasando por parametro el objeto req
-    fetch(req, {
-        method: 'GET',  
-        headers: headers
-    })
-    .then(response => response.json())
-    .then(data => {
-        const secNoticias = document.querySelector('div.noticias')
-        
-        //Aqui es donde cambiamos el contenido del html element
-        data.forEach(element => {
-            //ELEMENT ES EL CONTENIDO DE LA CONSULTA A LA API
-            //SE PUEDE AGREGAR MAS DATOS COMO USUARIO ETC.
+    const apiKey = 'ffc6693f2c1a4dccadbbb259a54dd21b';
+    const keyword = 'technology';
+    const language = 'es';
 
-            const noticia = document.createElement('div');
-            noticia.classList.add('noticia');
+    const url = `https://newsapi.org/v2/everything?q=tesla&from=2024-04-03&sortBy=publishedAt&apiKey=ffc6693f2c1a4dccadbbb259a54dd21b`
 
-            const noticiaImg = document.createElement('div');
-            noticiaImg.classList.add('noticia_img');
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const secNoticias = document.querySelector('div.noticias');
 
-            const noticiaImgImg = document.createElement('img');
-            noticiaImgImg.src = 'assets/img.png';
-            noticiaImgImg.alt = '';
+            // Itera no mas de 15 veces en la cantidad de articulos
+            data.articles.slice(0,15).forEach(article => {
+                const noticia = document.createElement('div');
+                noticia.classList.add('noticia');
 
-            noticiaImg.appendChild(noticiaImgImg);
+                const noticiaImg = document.createElement('div');
+                noticiaImg.classList.add('noticia_img');
 
-            const noticiaCuerpo = document.createElement('div');
-            noticiaCuerpo.classList.add('noticia_cuerpo');
+                const noticiaImgImg = document.createElement('img');
+                noticiaImgImg.src = article.urlToImage;
+                noticiaImgImg.alt = article.title;
+                noticiaImg.appendChild(noticiaImgImg);
 
-            const tituloNoticia = document.createElement('h1');
-            tituloNoticia.classList.add('titulo_noticia');
-            tituloNoticia.textContent = element.title;
+                const noticiaCuerpo = document.createElement('div');
+                noticiaCuerpo.classList.add('noticia_cuerpo');
 
-            const noticiaParrafo = document.createElement('p');
-            noticiaParrafo.textContent = element.body;
+                const tituloNoticia = document.createElement('h1');
+                tituloNoticia.classList.add('titulo_noticia');
+                tituloNoticia.textContent = article.title;
+                
+                const noticiaParrafo = document.createElement('p');
+                noticiaParrafo.textContent = article.description;
 
-            noticiaCuerpo.appendChild(tituloNoticia);
-            noticiaCuerpo.appendChild(noticiaParrafo);
+                noticiaCuerpo.appendChild(tituloNoticia);
+                noticiaCuerpo.appendChild(noticiaParrafo);
 
-            noticia.appendChild(noticiaImg);
-            noticia.appendChild(noticiaCuerpo);
-            secNoticias.appendChild(noticia);
-        });        
+                noticia.appendChild(noticiaImg);
+                noticia.appendChild(noticiaCuerpo);
+                secNoticias.appendChild(noticia);
+            });        
 
-    })
-    .catch(error => console.error('Error:', error));
+        })
+        .catch(error => console.error('Error:', error));
     
 });
