57b7d2f02b294d25f2d3c270ac0de4440ef43960
Hernan Gaston <hernangaston@gmail.com>
2024-05-19 17:35:52 -0300

    agregada imagen de viento

--- js/components/weather.js
+++ js/components/weather.js
@@ -6,11 +6,15 @@ class Clima extends HTMLElement {
     connectedCallback() {   
        this.innerHTML = `       
         <div class="climaComponent">
-            <img src="${this.attributes.condition_icon.value}" alt="${this.attributes.text.value}"/>
+            
             <div class="location"> ${this.attributes.location.value}</div>
             <div class="temp">${this.attributes.temp_c.value} ºC</div>
-            <div class="text">${this.attributes.text.value}</div>
-            <div class="wind">${this.attributes.viento.value} Kmh.</div>
+            
+            <div class="text">
+            <img src="${this.attributes.condition_icon.value}" alt="${this.attributes.text.value}"/>
+              ${this.attributes.text.value}
+            </div>
+            <div class="wind"><img src="../../assets/wind.png" alt="viento" /> ${this.attributes.viento.value} Kmh.</div>
         </div>
         `
     }
