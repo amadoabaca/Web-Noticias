03d3942bd60a5c584dbd52d9dff7c8c5b5e67091
Hernan Gaston <hernangaston@gmail.com>
2024-05-19 17:25:10 -0300

    barra de navegacion funcional

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
     const apiKey = 'ffc6693f2c1a4dccadbbb259a54dd21b';
-    const keyword = 'technology';
+    let keyword = 'technology';
     const language = 'es';
 
-    const url = `https://newsapi.org/v2/everything?q=${keyword}&language=${language}&sortBy=publishedAt&apiKey=${apiKey}`;
+    let url = `https://newsapi.org/v2/everything?q=${keyword}&language=${language}&sortBy=publishedAt&apiKey=${apiKey}`;
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const secNoticias = document.querySelector('div.noticias');
-        // Itera no mas de 15 veces en la cantidad de articulos
-        data.articles.slice(0,15).forEach(article => {            
-            if(article.urlToImage != null){
-                const noticia = document.createElement('my-news');
-                noticia.classList.add('noticia');
-                //MODIFICADO AHORA LLAMAMOS SOLO UN WEBCOMPONENT 'my-news' Y CAMBIAMOS EL VALOR DE LOS ATRIBUTOS                
-                noticia.setAttribute('image_source', article.urlToImage);
-                noticia.setAttribute('image_alt', article.title);
-                noticia.setAttribute('titulo', article.title);
-                noticia.setAttribute('cuerpo', article.description);
-                noticia.setAttribute('publishedAt', article.publishedAt);
-                secNoticias.appendChild(noticia);
-            }          
+    if(keyword == 'technology'){
+        llamarApiNoticias(url);
+    }
 
-        });                
+    
 
-    })
-    .catch(error => console.error('Error:', error));
+    function llamarApiNoticias(url){
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const secNoticias = document.querySelector('div.noticias');
+            // Itera no mas de 15 veces en la cantidad de articulos
+            data.articles.slice(0,15).forEach(article => {            
+                if(article.urlToImage != null){
+                    const noticia = document.createElement('my-news');
+                    noticia.classList.add('noticia');
+                    //MODIFICADO AHORA LLAMAMOS SOLO UN WEBCOMPONENT 'my-news' Y CAMBIAMOS EL VALOR DE LOS ATRIBUTOS                
+                    noticia.setAttribute('image_source', article.urlToImage);
+                    noticia.setAttribute('image_alt', article.title);
+                    noticia.setAttribute('titulo', article.title);
+                    noticia.setAttribute('cuerpo', article.description);
+                    noticia.setAttribute('publishedAt', article.publishedAt);
+                    secNoticias.appendChild(noticia);
+                }          
+
+            });                
+
+        })
+        .catch(error => console.error('Error:', error));
+    }
+
 
     //-------------------- LLAMADA A LA API DEL CLIMA --------------------
     const keyWeather = '867014d98631456eb4b91249241305'
@@ -53,5 +62,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
         sectionClima.appendChild(clima);})
     .catch(error => console.log(error));
+
+    document.getElementById('entretenimiento').addEventListener('click', () => {
+        console.log('entretenimiento');
+        keyword = 'entertainment';
+        const secNoticias = document.querySelector('div.noticias');
+        let noticias = document.querySelector('my-news');
+        secNoticias.removeChild(noticias);    
+        url = `https://newsapi.org/v2/everything?q=${keyword}&language=${language}&sortBy=publishedAt&apiKey=${apiKey}`;
+        llamarApiNoticias(url);    
+    });
+
+    document.getElementById('deportes').addEventListener('click', () => {
+        console.log('deportes');
+        keyword = 'sports';
+        const secNoticias = document.querySelector('div.noticias');
+        let noticias = document.querySelector('my-news');
+        secNoticias.removeChild(noticias);  
+        url = `https://newsapi.org/v2/everything?q=${keyword}&language=${language}&sortBy=publishedAt&apiKey=${apiKey}`;
+        llamarApiNoticias(url);      
+    });
+
+    document.getElementById('economia').addEventListener('click', () => {
+        console.log('economy');
+        keyword = 'economy';
+        const secNoticias = document.querySelector('div.noticias');
+        let noticias = document.querySelector('my-news');
+        secNoticias.removeChild(noticias);      
+        url = `https://newsapi.org/v2/everything?q=${keyword}&language=${language}&sortBy=publishedAt&apiKey=${apiKey}`;
+        llamarApiNoticias(url);  
+    });
+
+    document.getElementById('politica').addEventListener('click', () => {
+        console.log('politics');
+        keyword = 'politics';
+        const secNoticias = document.querySelector('div.noticias');
+        let noticias = document.querySelector('my-news');
+        secNoticias.removeChild(noticias);  
+        url = `https://newsapi.org/v2/everything?q=${keyword}&language=${language}&sortBy=publishedAt&apiKey=${apiKey}`;
+        llamarApiNoticias(url);      
+    });
+
     
 });
